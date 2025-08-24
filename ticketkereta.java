9eb7caf49abb26db21775be23d7d4cf71e5f43f6
Mason <bebek.logic@gmail.com>
2025-08-24 21:26:48 +0800

    tugas ticket kereta

--- ticketkereta.java
+++ ticketkereta.java
@@ -1,19 +1,25 @@
 package train_tickets;
 import java.util.Scanner;
 
-public class ticketkereta {
+public class ticketkereta 
+{
     static int funds;
 
-    public static void initialFunds(int BaseFunds){
+    public static void initialFunds(int BaseFunds)
+    {
         funds = BaseFunds;
     }
 
-    public static void tapIn(){
-        if (funds >= 10000) {
+    public static void tapIn()
+    {
+        if (funds >= 10000)
+        {
             System.out.println("Tap-In berhasil. Selamat menikmati perjalanan");
             funds -= 3000;
             System.out.println("Sisa saldo anda saat sekarang: Rp. " + funds);
-        } else {
+        }
+        else
+        {
             System.out.println("Saldo anda tidak cukup.");
             System.out.println("Saldo anda sekarang: Rp. " + funds);
             System.out.println("Silahkan isi ulang kartu anda.");
@@ -21,7 +27,8 @@ public class ticketkereta {
         }
     }
 
-    public static void refill(){
+    public static void refill()
+    {
         Scanner input = new Scanner(System.in);
         System.out.print("Masukkan jumlah top-up saldo anda: Rp. ");
         int topUp = input.nextInt();
@@ -31,7 +38,8 @@ public class ticketkereta {
         input.close();
     }
     
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+    {
         initialFunds(5000);
         tapIn();
     }
