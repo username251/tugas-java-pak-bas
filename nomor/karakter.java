afa73b4a59439dd8a3bab607e995649cf12c0c7c
Mason <81406803+username251@users.noreply.github.com>
2025-10-19 22:44:25 +0800

    ngantuk

---
+++ nomor/karakter.java
@@ -0,0 +1,27 @@
+
+public class karakter {
+    private String nama;
+    private int hp;
+
+    public karakter(String nama, int hp) {
+        this.nama = nama;
+        this.hp = hp;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void diserang(int damage) {
+        hp -= damage;
+        System.out.println(nama + " menerima " + damage + " damage");
+    }
+
+    public void bergerak() {
+        System.out.println(nama + " bergerak ke depan.");
+    }
+}
