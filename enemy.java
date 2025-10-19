1a4944cb112ab622a884e37bfbb39cc74e5b1dd5
Mason <bebek.logic@gmail.com>
2025-10-19 22:37:31 +0800

    ngantuk

---
+++ enemy.java
@@ -0,0 +1,12 @@
+package nomor;
+
+public class enemy extends karakter {
+    public enemy(String nama, int hp) {
+        super(nama, hp);
+    }
+
+    @Override
+    public void bergerak() {
+        System.out.println(getNama() + " berjalan ke arah player");
+    }
+}
---
+++ utama.java
@@ -0,0 +1,17 @@
+package nomor;
+
+public class utama {
+    public class Main {
+    public static void main(String[] args) {
+        player player1 = new player("Alex", 100, 0);
+        enemy enemy1 = new enemy("Zombie", 80);
+
+        player1.bergerak();  // hasil: Alex berlari cepat!
+        enemy1.bergerak();  // hasil: Zombie berjalan lambat ke arah pemain...
+
+        enemy1.diserang(20);
+        player1.tambahSkor(10);
+    }
+}
+
+}
